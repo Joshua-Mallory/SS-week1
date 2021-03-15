dce2703fc93494761e322beb64f0bfd044915ac4
jcmal <jcmal@DESKTOP-6OST5PC>
2021-03-15 08:19:59 -0400

    Functional Example

--- SSWeek1/src/com/ss/mar/day5/project/LambdaFunction.java
+++ SSWeek1/src/com/ss/mar/day5/project/LambdaFunction.java
@@ -2,6 +2,5 @@ package com.ss.mar.day5.project;
 
 @FunctionalInterface
 public interface LambdaFunction {
-
-	boolean i(int i);
+	Boolean calculate(Integer x);
 }
--- SSWeek1/src/com/ss/mar/day5/project/Lambdas.java
+++ SSWeek1/src/com/ss/mar/day5/project/Lambdas.java
@@ -22,8 +22,11 @@ public class Lambdas {
 	}
 
 	public String evenOrOddArg(Integer temp1) {
+
 		if (temp1 != null) {
-			if (temp1 % 2 == 0)
+			// functional Interface
+			LambdaFunction lf = temp2 -> (temp2 % 2) == 0;
+			if (lf.calculate(temp1))
 				return temp1 + " " + "EVEN";
 
 			return temp1 + " " + "ODD";
@@ -85,9 +88,10 @@ public class Lambdas {
 
 	public List<String> lambdaWalk(List<Integer> myCases, List<Integer> myValues) {
 		if (myCases != null && myValues != null) {
-
+			AtomicInteger counter1 = new AtomicInteger(-1);
+			Function<Integer, String> fun = temp1 -> switcher(temp1, myValues, counter1);
 			AtomicInteger counter = new AtomicInteger(-1);
-			Function<Integer, String> fun = (temp1 -> switcher(temp1, myValues, counter));
+
 			List<String> allList = myCases.stream().map(fun).collect(Collectors.toList());
 			allList.forEach(System.out::println);
 			return allList;
